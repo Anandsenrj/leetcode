e8527db892fd4c62ec2deb10cb3dbb6e4912f524
Anand Sen <84634909+Anandsenrj@users.noreply.github.com>
2026-02-06 01:44:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (43.25%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-         double solution= pow(x,n);
-         return solution;
+        long long N = n;   // prevent overflow
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        double result = 1.0;
+        while (N > 0) {
+            if (N % 2 == 1)
+                result *= x;
+            x *= x;
+            N /= 2;
+        }
+        return result;
     }
 };
