9280c35f4dc177ec93e1d63df6dd43318b08802f
Anand Sen <84634909+Anandsenrj@users.noreply.github.com>
2026-07-08 12:04:58 +0530

    Time: 7 ms (5.31%) | Memory: 27.3 MB (17.60%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,27 +1,66 @@
+// class Solution {
+// public:
+// int trap(vector<int>& height) {
+// int n = height.size();
+// int water = 0;
+
+// for(int j = 0; j < n; j++) {
+// int leftMax = 0, rightMax = 0;
+
+// }
+// // find max on left
+
+// for(int i = 0; i <= j; i++) {
+
+// leftMax = max(leftMax, height[i]);
+// }
+// // find max on right
+
+// for(int r = j; r < n; r++) {
+
+// rightMax = max(rightMax, height[r]);
+
+// }
+
+// // water at index i
+
+// water += min(leftMax, rightMax) - height[j];
+
+// return water;
+
+// }
+// };
+
+
 class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        stack<int> st;
+
+        if (n == 0)
+            return 0;
+
+        vector<int> prefix(n), suffix(n);
+
+        // Prefix maximum
+        prefix[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            prefix[i] = max(prefix[i - 1], height[i]);
+        }
+
+        // Suffix maximum
+        suffix[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffix[i] = max(suffix[i + 1], height[i]);
+        }
+
+        // Calculate trapped water
         int ans = 0;
-
         for (int i = 0; i < n; i++) {
-            while (!st.empty() && height[i] > height[st.top()]) {
-                int mid = st.top();
-                st.pop();
-
-                if (st.empty()) break;
-
-                int left = st.top();
-                int width = i - left - 1;
-                int boundedHeight =
-                    min(height[left], height[i]) - height[mid];
-
-                ans += width * boundedHeight;
-            }
-            st.push(i);
+            ans += min(prefix[i], suffix[i]) - height[i];
         }
 
         return ans;
     }
 };
+                
