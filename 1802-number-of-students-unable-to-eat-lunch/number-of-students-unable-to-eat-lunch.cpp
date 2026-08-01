5d4306bb05543c643eb80e43a5de0e447cba06ad
Anand Sen <84634909+Anandsenrj@users.noreply.github.com>
2026-08-02 01:41:35 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (91.47%) - LeetSync

---
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        int n=students.size();
+        int count_1=0;
+        int count_0=0;
+        for(int i=0;i<n;i++){
+            if(students[i]==1){
+                count_1++;
+            }                
+            else{
+                count_0++;
+            }
+        }
+        
+        for(int j=0;j<n;j++){
+            if(sandwiches[j]==1){
+                if(count_1==0){
+                    return count_0;
+                }
+                count_1--;
+            }
+            else{
+                if(count_0==0){
+                    return count_1;
+                }
+                count_0--;
+            }
+                
+            
+        }
+        //      for (int sandwich : sandwiches) {
+        //     if (sandwich == 0) {
+        //         if (count_0 == 0)
+        //             return count_1;
+        //         count_0--;
+        //     } else {
+        //         if (count_1 == 0)
+        //             return count_0;
+        //         count_1--;
+        //     }
+        // }
+
+        
+        return 0;
+        }
+    };
