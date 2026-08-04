4fb03596be915d07410b7892c7f7954ea20f9730
Anand Sen <84634909+Anandsenrj@users.noreply.github.com>
2026-08-05 01:48:29 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (75.34%) - LeetSync

---
+++ 983-validate-stack-sequences/validate-stack-sequences.cpp
@@ -0,0 +1,36 @@
+// class Solution {
+// public:
+//     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+//         stack<int> st;
+//         int m = pushed.size();
+//         int i = 0,j=0;
+
+//         while(i<m  && j <m){
+
+//             while (!st.empty() && j<m && st.top() == popped[j]) {
+//                 st.pop();
+//                 j++;
+//             }
+//         }
+
+//         return st.empty();
+//     }
+// };
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int> st;
+        int j = 0;
+
+        for (int num : pushed) {
+            st.push(num);
+
+            while (!st.empty() && st.top() == popped[j]) {
+                st.pop();
+                j++;
+            }
+        }
+
+        return st.empty();
+    }
+};
