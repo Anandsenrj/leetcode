b67385d14f6cd8bc37ffd8baa8dee518074139e6
Anand Sen <84634909+Anandsenrj@users.noreply.github.com>
2026-08-06 23:50:25 +0530

    Time: 2 ms (12.75%) | Memory: 19.2 MB (13.48%) - LeetSync

--- 983-validate-stack-sequences/validate-stack-sequences.cpp
+++ 983-validate-stack-sequences/validate-stack-sequences.cpp
@@ -1,13 +1,32 @@
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int> st;
+        int m = pushed.size();
+        int i = 0,j=0;
+
+        while(i<m  && j <m){
+                st.push(pushed[i]);
+            while (!st.empty() && j<m && st.top() == popped[j]) {
+                st.pop();
+                j++;
+            }
+                i++;
+        }
+
+        return st.empty();
+    }
+};
 // class Solution {
 // public:
 //     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
 //         stack<int> st;
-//         int m = pushed.size();
-//         int i = 0,j=0;
+//         int j = 0;
 
-//         while(i<m  && j <m){
+//         for (int num : pushed) {
+//             st.push(num);
 
-//             while (!st.empty() && j<m && st.top() == popped[j]) {
+//             while (!st.empty() && st.top() == popped[j]) {
 //                 st.pop();
 //                 j++;
 //             }
@@ -16,21 +35,3 @@
 //         return st.empty();
 //     }
 // };
-class Solution {
-public:
-    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
-        stack<int> st;
-        int j = 0;
-
-        for (int num : pushed) {
-            st.push(num);
-
-            while (!st.empty() && st.top() == popped[j]) {
-                st.pop();
-                j++;
-            }
-        }
-
-        return st.empty();
-    }
-};
