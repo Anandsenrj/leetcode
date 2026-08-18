fe7eeb2fb5208687e8ce8d0939ca86a5a9b54133
Anand Sen <84634909+Anandsenrj@users.noreply.github.com>
2026-08-18 22:12:11 +0530

    Time: 2 ms (72.63%) | Memory: 44.9 MB (69.12%) - LeetSync

--- 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
@@ -1,14 +1,29 @@
 class Solution {
-    public int largestInteger(int[] A, int k) {
-        int[] f = new int[51];
-        for (int x : A)
-            f[x]++;
+    public int largestInteger(int[] nums, int k) {
+        int [] count = new int [51];
 
-        int res = -1, n = A.length;
-        for (int i = 0; i < n; i++) 
-            if (k == n || (f[A[i]] == 1 && (k == 1 || i == 0 || i == n - 1)))
-                res = Math.max(res, A[i]);
+        //checking every sub array of size k
+        for(int i= 0;i<=nums.length-k;i++){
+            boolean[] seen =new boolean[51];
+            for(int j =i;j<i+k;j++){
+                seen[nums[j]]=true;
+            }
 
-        return res;
+            //Counting this subarray only once for each number
+            for(int x=0;x<=50;x++){
+                if(seen[x]){
+                    count[x]++;
+                }
+            }
+        }
+
+        //finding largest number appearing in exactly one sunarray
+
+        for(int x=50;x>=0;x--){
+            if(count[x]==1){
+                return x;
+            }
+        }
+        return -1;
     }
 }
