58ec2be19e3b3a4d5713d71138ebb73512dc33a5
Anand Sen <84634909+Anandsenrj@users.noreply.github.com>
2026-09-05 13:21:58 +0530

    Time: 1100 ms (94.27%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,2 +1,2 @@
-/* Write your T-SQL query statement below */
+-- Write your PostgreSQL query statement below
 select product_name,year,price from sales s left join product p on s.product_id=p.product_id ;
