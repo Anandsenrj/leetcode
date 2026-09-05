590abae15f2c90b68880087bc9f03d8e44c283ed
Anand Sen <84634909+Anandsenrj@users.noreply.github.com>
2026-09-05 11:07:00 +0530

    Time: 700 ms (64.91%) | Memory: 0B (100.00%) - LeetSync

--- 1136-actors-and-directors-who-cooperated-at-least-three-times/actors-and-directors-who-cooperated-at-least-three-times.sql
+++ 1136-actors-and-directors-who-cooperated-at-least-three-times/actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -1,2 +1,2 @@
-# Write your MySQL query statement below
+/* Write your T-SQL query statement below */
 select actor_id,director_id from actordirector group by actor_id,director_id having count(timestamp)>=3;
